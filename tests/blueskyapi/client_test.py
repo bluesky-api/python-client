b875583f9e85f24268462de838972a01412eaf72
Max Hollmann <max@blueskyapi.io>
2021-12-27 23:47:49 +0100

    Use pytest-describe

--- tests/blueskyapi/client_test.py
+++ tests/blueskyapi/client_test.py
@@ -6,24 +6,6 @@ import responses
 import blueskyapi
 
 
-class TestInit:
-    def test_defaults(self, mocker):
-        mocker.patch.multiple(
-            "blueskyapi.default_config",
-            base_url="the base url",
-            api_key="the api key",
-        )
-
-        client = blueskyapi.Client()
-        assert client.base_url == "the base url"
-        assert client.api_key == "the api key"
-
-    def test_with_args(self):
-        client = blueskyapi.Client("api-key", base_url="https://example.com/api")
-        assert client.base_url == "https://example.com/api"
-        assert client.api_key == "api-key"
-
-
 @pytest.fixture()
 def client():
     return blueskyapi.Client()
@@ -41,67 +23,72 @@ def add_api_response(path, *, result=default_result, status=200):
     )
 
 
-class TestLatestForecast:
+def describe_init():
+    def test_defaults(mocker):
+        mocker.patch.multiple(
+            "blueskyapi.default_config",
+            base_url="the base url",
+            api_key="the api key",
+        )
+
+        client = blueskyapi.Client()
+        assert client.base_url == "the base url"
+        assert client.api_key == "the api key"
+
+    def test_with_args():
+        client = blueskyapi.Client("api-key", base_url="https://example.com/api")
+        assert client.base_url == "https://example.com/api"
+        assert client.api_key == "api-key"
+
+
+def describe_latest_forecast():
     @responses.activate
-    def test_defaults(self, client):
+    def test_defaults(client):
         add_api_response("/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5")
         client.latest_forecast(53.5, 13.5)
 
-    @responses.activate
-    def test_with_distances_as_array(self, client):
-        add_api_response(
-            "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5&prediction_distances=0,24"
-        )
-        client.latest_forecast(53.5, 13.5, prediction_distances=[0, 24])
+    def describe_prediction_distances():
+        @responses.activate
+        def with_array(client):
+            add_api_response(
+                "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5&prediction_distances=0,24"
+            )
+            client.latest_forecast(53.5, 13.5, prediction_distances=[0, 24])
+
+        @responses.activate
+        def with_string(client):
+            add_api_response(
+                "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5&prediction_distances=0,24"
+            )
+            client.latest_forecast(53.5, 13.5, prediction_distances="0,24")
+
+        @responses.activate
+        def with_invalid_value(client):
+            with pytest.raises(TypeError, match="prediction_distances should be"):
+                client.latest_forecast(53.5, 13.5, prediction_distances=1.5)
+
+    def describe_columns():
+        @responses.activate
+        def with_array(client):
+            add_api_response(
+                "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5&columns=col_a,col_b"
+            )
+            client.latest_forecast(53.5, 13.5, columns=["col_a", "col_b"])
+
+        @responses.activate
+        def with_string(client):
+            add_api_response(
+                "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5&columns=col_a,col_b"
+            )
+            client.latest_forecast(53.5, 13.5, columns="col_a,col_b")
+
+        @responses.activate
+        def with_invalid_value(client):
+            with pytest.raises(TypeError, match="columns should be"):
+                client.latest_forecast(53.5, 13.5, columns=1)
 
     @responses.activate
-    def test_with_distances_as_string(self, client):
-        add_api_response(
-            "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5&prediction_distances=0,24"
-        )
-        client.latest_forecast(53.5, 13.5, prediction_distances="0,24")
-
-    @responses.activate
-    def test_with_invalid_distances(self, client):
-        with pytest.raises(TypeError, match="prediction_distances should be"):
-            client.latest_forecast(53.5, 13.5, prediction_distances=1.5)
-
-    @responses.activate
-    def test_with_columns_as_array(self, client):
-        add_api_response(
-            "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5&columns=col_a,col_b"
-        )
-        client.latest_forecast(53.5, 13.5, columns=["col_a", "col_b"])
-
-    @responses.activate
-    def test_with_columns_as_string(self, client):
-        add_api_response(
-            "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5&columns=col_a,col_b"
-        )
-        client.latest_forecast(53.5, 13.5, columns="col_a,col_b")
-
-    @responses.activate
-    def test_with_invalid_columns(self, client):
-        with pytest.raises(TypeError, match="columns should be"):
-            client.latest_forecast(53.5, 13.5, columns=1)
-
-    @responses.activate
-    def test_with_selection(self, client):
-        add_api_response(
-            "/forecasts/gfs_0p25/latest"
-            "?lat=53.5&lon=13.5"
-            "&prediction_distances=0,24"
-            "&columns=apparent_temperature_at_2m"
-        )
-        client.latest_forecast(
-            53.5,
-            13.5,
-            prediction_distances=[0, 24],
-            columns=["apparent_temperature_at_2m"],
-        )
-
-    @responses.activate
-    def test_over_rate_limit(self, client):
+    def test_over_rate_limit(client):
         add_api_response(
             "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5",
             result={"the": "result"},
@@ -111,7 +98,7 @@ class TestLatestForecast:
             client.latest_forecast(53.5, 13.5)
 
     @responses.activate
-    def test_result(self, client):
+    def test_result(client):
         add_api_response(
             "/forecasts/gfs_0p25/latest?lat=53.5&lon=13.5",
             result=[{"prediction_moment": "2021-12-27T18:00:00Z", "some_column": 5}],
@@ -123,7 +110,7 @@ class TestLatestForecast:
         assert np.all(result.some_column == [5])
 
     @pytest.mark.vcr()
-    def test_integration(self, client):
+    def test_integration(client):
         result = client.latest_forecast(53.5, 13.5)
 
         assert len(result.columns) == 35
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,11 @@
+import os
+
 import pytest
 
 
 pytest_plugins = []
+
+
+@pytest.fixture(scope="module")
+def vcr_cassette_dir(request):
+    return os.path.join("cassettes", request.module.__name__)
